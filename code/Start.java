80023d63d9edb523dd087d512ef99d655f308315
Voragun Supawong <voragun2543@gmail.com>
2022-08-25 21:31:53 +0700

    Knapsack problem
    The easiest hard problem

--- code/Start.java
+++ code/Start.java
@@ -1,11 +1,34 @@
 class Start {
     public static void main(String[] data) {
-        int value = 8;
-	int[] stamp = { 5, 4, 1 };      // must be sorted in descending order
-	for(int i = 0; i < stamp.length; i++) {
-            int use = value / stamp[i];
-            System.out.println("Stamp " + stamp[i] + " use " + use + " item(s).");
-            value = value - use * stamp[i];
-	}
+        int value = 12;
+	int[] stamp = { 1, 4, 5 };      // must be sorted in descending order
+	
+        int[][] best = new int[value + 1][stamp.length];
+        // fill the first column (column 0)
+        for(int i = 0; i < best.length; i++) {
+            best[i][0] = i;
+            //      '------------> first column            
+        }
+        
+        // fill column-by-column
+        for(int c = 1; c < stamp.length; c++) {
+            for (int i = 0; i < best.length; i++) {
+                // choose the best between left cell, top call
+                int left = best[i][c-1];
+                int last_layer = i - stamp[c];
+                if (last_layer < 0) {
+                    // there is only one choice: the left cell
+                    best[i][c] = left;
+                }
+                if (last_layer >= 0) {
+                    // there are two choices: left and above
+                    int above = best[last_layer][c] + 1;
+                                // Use 1 stamp -------'
+                    best[i][c] = left < above ? left : above;
+                }
+            }           
+        }
+        System.out.println(best[8][2]);   // 2 --> 4 + 4
+        System.out.println(best[10][2]);  // 2 --> 5 + 5
     }
 }
