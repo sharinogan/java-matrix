04b1af6ee1974328319880eb408be86a1e6da09a
Voragun Supawong <voragun2543@gmail.com>
2022-08-25 20:26:18 +0700

    Given a square matrix, write code to print all cells in main diagonal

--- code/Start.java
+++ code/Start.java
@@ -1,22 +1,17 @@
 class Start {
 	public static void main(String[] data) {
 		double[][] map = new double[4][5];      // value of each cell is 0.0
-		map[0][1] = 7.6;
-		map[1][1] = 7.9;
-		map[2][1] = 8.5;
-		map[3][1] = 8.3;
+ 
+                // Given a square matrix, write code
+                // to print all cells in main diagonal
+                double[][] m = new double[37][37];
+                map[0][1] = 9.2;
+		map[1][1] = 8.7;
+		map[2][2] = 8.9;
 
-		System.out.println(map[2][1]);    // 8.5
-		System.out.println(map[1][3]);    // 0.0
-
-		// Given a 4x5 matrix, write code to find the sum
-		// of each cell in column 1
-                double result = 0.0;
                 for (int i = 0; i < map.length; i++) {
-                    result += map[i][1];
+                    System.out.println(map[i][i]);
                 }
-                
-                System.out.println("Result is " + result);
 
 	}
 }
